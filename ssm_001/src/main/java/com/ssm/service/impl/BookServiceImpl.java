d0534ff4b0cbb9483a28f8a152c4cd8152060e4e
sha-mo-xuan-shen <2052039701@qq.com>
2023-04-12 14:25:16 +0800

    记得先关掉自己加的异常

--- ssm_001/src/main/java/com/ssm/service/impl/BookServiceImpl.java
+++ ssm_001/src/main/java/com/ssm/service/impl/BookServiceImpl.java
@@ -35,11 +35,11 @@ public class BookServiceImpl implements BookService {
 
     public Book getById(int id) {
 
-        if(id==1){
-            throw new BussinessException(Code.BUSSINESS_ERR,"找不到1");
-        }
+        // if(id==1){
+        //     throw new BussinessException(Code.BUSSINESS_ERR,"找不到1");
+        // }
         try {
-            int i = 1/0;
+            // int i = 1/0;
         }catch (Exception e){
             throw new SystemException(Code.SYSTEM_TIMEOUT,"查询失败，访问超时");
         }
@@ -50,7 +50,7 @@ public class BookServiceImpl implements BookService {
     public List<Book> getAll() {
 
         try {
-            int i = 1/0;
+            // int i = 1/0;
         }catch (ArithmeticException e){
             throw new SystemException(Code.SYSTEM_ERR,"查询失败，请重试",e);
         }
